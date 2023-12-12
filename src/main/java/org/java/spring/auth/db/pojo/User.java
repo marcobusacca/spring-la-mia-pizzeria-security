76823319636c3754849c84d4ee43e083cc5d3da4
marcobusacca <marcobusacca01@gmail.com>
2023-12-12 12:10:44 +0100

    [FEAT] mvc auth relation between "User" and "Role"

--- src/main/java/org/java/spring/auth/db/pojo/User.java
+++ src/main/java/org/java/spring/auth/db/pojo/User.java
@@ -1,15 +1,19 @@
 package org.java.spring.auth.db.pojo;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class User implements UserDetails {
@@ -24,11 +28,15 @@ public class User implements UserDetails {
 	@Column(nullable = false)
 	private String password;
 	
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<Role> roles;
+	
 	public User() { }
-	public User(String username, String password) {
+	public User(String username, String password, Role... roles) {
 		
 		setUsername(username);
 		setPassword(password);
+		setRoles(roles);
 	}
 	
 	public int getId() {
@@ -49,11 +57,22 @@ public class User implements UserDetails {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public List<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	public void setRoles(Role... roles) {
+		setRoles(List.of(roles));
+	}
 	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return getRoles().stream()
+				.map(r -> new SimpleGrantedAuthority(r.getName()))
+			.toList();
 	}
 	@Override
 	public boolean isAccountNonExpired() { return true; }
